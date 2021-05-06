f79edb6586d5cf4540262c5e7651980830d61bfd
jorgepavez <jorge.pavez@outlook.com>
2021-05-06 19:44:24 -0400

    mejora de errores

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,26 +14,3 @@ $(document).ready(function() {
         }
     });
 });
-/* $(function() {
-    $('[data-toggle="tooltip"]').tooltip()
-})
-
-$("#enviarCorreo").click(function() {
-    alert("El correo fue enviado correctamente...");
-});
-
-$("#ingrediente").on({
-    dblclick: function() {
-        $(this).addClass("rojo");
-    }
-});
-
-$("#preparacion").on({
-    dblclick: function() {
-        $(this).addClass("rojo");
-    }
-});
-
-$("#titulo-tarjeta").click(function() {
-    $("#tarjetas").toggle();
-}); */
